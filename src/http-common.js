a463323a425b32625db78c5bcbdad07ee61a6849
jacksontate <jackson_tate@outlook.com>
2023-01-12 12:52:55 -0600

    courses fix

--- src/http-common.js
+++ src/http-common.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: process.env.NODE_ENV === 'development' ? "http://localhost/courselist" : "nodeapps/2022/jacksonbackend",
+  baseURL: process.env.NODE_ENV === 'development' ? "http://localhost/" : "/course-jt",
   headers: { "Content-Type": "application/json" }
 });
