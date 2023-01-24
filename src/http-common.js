865822d2e9447df2674a7d8779584394a5b10e44
jacksontate <jackson_tate@outlook.com>
2023-01-23 20:57:06 -0600

    updated http common to work with local

--- src/http-common.js
+++ src/http-common.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: process.env.NODE_ENV === 'development' ? "http://localhost/" : "/course-jt",
+  baseURL: process.env.NODE_ENV === 'development' ? "http://localhost:3027/course-jt" : "/course-jt",
   headers: { "Content-Type": "application/json" }
 });
