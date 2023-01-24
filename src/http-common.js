b7560cdf478b3b8b130b32e255e6295b3e6e07d7
jacksontate <jackson_tate@outlook.com>
2023-01-23 21:07:17 -0600

    PRAY THIS WORKS ON AWS

--- src/http-common.js
+++ src/http-common.js
@@ -1,5 +1,28 @@
 import axios from "axios";
-export default axios.create({
-  base: process.env.NODE_ENV === 'development' ? "http://localhost/course-jt/" : "/course-jt",
-  headers: { "Content-Type": "application/json" }
-});
+
+var AXIOS = {};
+if( process.env.NODE_ENV === 'development' ){
+  AXIOS = {
+    baseURL: "http://localhost:3027/course-jt/",
+    headers: { "Content-Type": "application/json" }
+  }
+} else {
+  AXIOS =  {
+    base: "/course-jt",
+    headers: { "Content-Type": "application/json" }
+  }
+}
+
+export default axios.create(AXIOS);
+
+/*** works locally ***/
+// export default axios.create({
+//   baseURL: process.env.NODE_ENV === 'development' ? "http://localhost:3027/course-jt/" : "/course-jt",
+//   headers: { "Content-Type": "application/json" }
+// });
+
+/*** works aws ***/
+// export default axios.create({
+//   base: process.env.NODE_ENV === 'development' ? "http://localhost:3027/course-jt/" : "/course-jt",
+//   headers: { "Content-Type": "application/json" }
+// });
